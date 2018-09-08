53fd4b10c9cb2e0251759ee0477a8c309d73bbbc
Alexander Pristavko <pristavkoal@gmail.com>
2018-09-08 20:21:58 +0300

    Move target

---
+++ VisionAR/VisionAR.h
@@ -0,0 +1,19 @@
+//
+//  VisionAR.h
+//  VisionAR
+//
+//  Created by Alexander Pristavko on 9/8/18.
+//  Copyright © 2018 Mapbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VisionAR.
+FOUNDATION_EXPORT double VisionARVersionNumber;
+
+//! Project version string for VisionAR.
+FOUNDATION_EXPORT const unsigned char VisionARVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisionAR/PublicHeader.h>
+
+
